09b8a10c219f3d9deedbc17f8871bed745655f2d
YangKian <1207783292@qq.com>
2022-02-18 14:49:50 +0800

    implement append

--- hstream/stream.go
+++ hstream/stream.go
@@ -4,61 +4,34 @@ import (
 	"client/client"
 	hstreampb "client/gen-proto/hstream/server"
 	"client/hstreamrpc"
+	"client/util"
 	"context"
 	"fmt"
+	"github.com/pkg/errors"
+	"go.uber.org/zap"
 	"math/rand"
+	"strconv"
 	"time"
 )
 
 const DEFAULTAPPENDTIMEOUT = time.Second * 5
 
-type AppenderOpt func(appender *Appender)
-
-type Appender struct {
-	enableBatch bool
-	batchSize   int32
-	timeOut     time.Duration
-}
-
-func EnableBatch(batchSize int32) AppenderOpt {
-	return func(appender *Appender) {
-		appender.enableBatch = true
-		appender.batchSize = batchSize
-	}
-}
-
-func TimeOut(timeOut time.Duration) AppenderOpt {
-	return func(appender *Appender) {
-		appender.timeOut = timeOut
-	}
-}
-
-func newDefaultAppender() *Appender {
-	return &Appender{
-		enableBatch: false,
-		batchSize:   1,
-		timeOut:     DEFAULTAPPENDTIMEOUT,
-	}
-}
-
-func newAppender(opts ...AppenderOpt) *Appender {
-	appender := newDefaultAppender()
-	for _, opt := range opts {
-		opt(appender)
-	}
-	return appender
+type appendEntry struct {
+	key        string
+	value      client.HStreamRecord
+	streamName string
+	res        *hstreamrpc.RPCAppendRes
 }
 
 type Stream struct {
-	Appender
-	client client.Client
+	writers map[string]*StreamProducer // FIXME: use Sync.Map to ensure thread safe ???
+	client  client.Client
 }
 
-func NewStream(client client.Client, opts ...AppenderOpt) *Stream {
-	if len(opts) == 0 {
-		return &Stream{Appender: *newDefaultAppender(), client: client}
-	} else {
-		return &Stream{Appender: *newAppender(opts...), client: client}
+func NewStream(client client.Client) *Stream {
+	return &Stream{
+		client:  client,
+		writers: make(map[string]*StreamProducer),
 	}
 }
 
@@ -67,7 +40,7 @@ func (s *Stream) Create(ctx context.Context, streamName string, replicationFacto
 		StreamName:        streamName,
 		ReplicationFactor: replicationFactor,
 	}
-	address, err := s.randomServer()
+	address, err := randomServer(s.client)
 	if err != nil {
 		return err
 	}
@@ -84,7 +57,7 @@ func (s *Stream) Create(ctx context.Context, streamName string, replicationFacto
 }
 
 func (s *Stream) Delete(ctx context.Context, streamName string) error {
-	address, err := s.randomServer()
+	address, err := randomServer(s.client)
 	if err != nil {
 		return err
 	}
@@ -103,7 +76,7 @@ func (s *Stream) Delete(ctx context.Context, streamName string) error {
 }
 
 func (s *Stream) List(ctx context.Context) (*client.StreamIter, error) {
-	address, err := s.randomServer()
+	address, err := randomServer(s.client)
 	if err != nil {
 		return nil, err
 	}
@@ -121,8 +94,212 @@ func (s *Stream) List(ctx context.Context) (*client.StreamIter, error) {
 	return client.NewStreamIter(streams), nil
 }
 
-func (s *Stream) LookUp(ctx context.Context, streamName string, key string) (string, error) {
-	address, err := s.randomServer()
+func (s *Stream) Append(ctx context.Context, streamName string, key string, tp client.RecordType, data []byte) (client.AppendResult, error) {
+	panic("not implemented")
+}
+
+func (s *Stream) MakeProducer(streamName string, key string, opts ...client.ProducerOpt) client.StreamProducer {
+	pId := streamName + ":" + key
+	if producer, ok := s.writers[pId]; ok {
+		return producer
+	}
+
+	producer := newStreamProducer(s.client, streamName, key, opts...)
+	if producer.enableBatch {
+		producer.dataCh = make(chan *appendEntry, producer.batchSize)
+		go producer.batchAppendLoop()
+	}
+	s.writers[pId] = producer
+	util.Logger().Info("new producer", zap.String("streamName", streamName), zap.String("key", key))
+	return producer
+}
+
+func randomServer(client client.Client) (string, error) {
+	infos, err := client.GetServerInfo()
+	idx := rand.Intn(len(infos))
+	if err != nil {
+		return "", err
+	}
+	return infos[idx], nil
+}
+
+type StreamProducer struct {
+	streamName  string
+	key         string
+	client      client.Client
+	enableBatch bool
+	batchSize   int
+	buffer      []*appendEntry
+	timeOut     time.Duration
+	isClosed    bool
+
+	dataCh chan *appendEntry
+	stop   chan struct{}
+}
+
+func newStreamProducer(client client.Client, streamName string, key string, opts ...client.ProducerOpt) *StreamProducer {
+	producer := &StreamProducer{
+		streamName:  streamName,
+		key:         key,
+		client:      client,
+		enableBatch: false,
+		batchSize:   0,
+		timeOut:     10 * time.Second,
+		isClosed:    false,
+		stop:        make(chan struct{}),
+	}
+
+	for _, opt := range opts {
+		opt(producer)
+	}
+	return producer
+}
+
+// EnableBatch enable batch append, if batchSize <= 0, convert to
+// single append by default
+func EnableBatch(batchSize int) client.ProducerOpt {
+	return func(producer client.StreamProducer) {
+		if batchSize <= 0 {
+			util.Logger().Warn("batch size must be greater than 0")
+			return
+		}
+
+		p := producer.(*StreamProducer)
+		p.enableBatch = true
+		p.batchSize = batchSize
+		p.buffer = make([]*appendEntry, 0, batchSize)
+	}
+}
+
+func TimeOut(timeOut time.Duration) client.ProducerOpt {
+	return func(producer client.StreamProducer) {
+		p := producer.(*StreamProducer)
+		p.timeOut = timeOut
+	}
+}
+
+func (p *StreamProducer) Stop() {
+	defer func() {
+		for i := 0; i < len(p.buffer); i++ {
+			p.buffer[i] = nil
+		}
+		p.buffer = p.buffer[:0]
+	}()
+	util.Logger().Info("producer stopped", zap.String("streamName", p.streamName), zap.String("key", p.key))
+	p.isClosed = true
+	close(p.stop)
+}
+
+func (p *StreamProducer) Append(tp client.RecordType, data []byte) client.AppendResult {
+	res := hstreamrpc.NewRPCAppendRes()
+	var record client.HStreamRecord
+	switch tp {
+	case client.RAWRECORD:
+		record = client.NewRawRecord(p.key, data)
+	case client.HRECORD:
+		record = client.NewHRecord(p.key, data)
+	default:
+		res.Err = errors.New("unsupported record type")
+		return res
+	}
+
+	entry := &appendEntry{
+		streamName: p.streamName,
+		key:        p.key,
+		value:      record,
+		res:        res,
+	}
+
+	if p.enableBatch {
+		p.dataCh <- entry
+		return res
+	}
+
+	p.sendAppend([]*appendEntry{entry})
+	return res
+}
+
+func (p *StreamProducer) fetchBatchData() []*appendEntry {
+	timer := time.NewTimer(p.timeOut)
+	defer timer.Stop()
+	p.buffer = p.buffer[:0]
+
+	for {
+		select {
+		case record := <-p.dataCh:
+			p.buffer = append(p.buffer, record)
+			if len(p.buffer) >= p.batchSize {
+				if !timer.Stop() {
+					<-timer.C
+				}
+				res := make([]*appendEntry, p.batchSize)
+				copy(res, p.buffer)
+				return res
+			}
+		case <-timer.C:
+			util.Logger().Debug("Timeout!!!!!!!", zap.String("length of buffer", strconv.Itoa(len(p.buffer))))
+			size := len(p.buffer)
+			if size == 0 {
+				return nil
+			}
+			res := make([]*appendEntry, size)
+			copy(res, p.buffer)
+			return res
+		case <-p.stop:
+			return nil
+		}
+	}
+}
+
+func (p *StreamProducer) batchAppendLoop() {
+	for {
+		if p.isClosed {
+			return
+		}
+
+		records := p.fetchBatchData()
+		if len(records) == 0 {
+			continue
+		}
+		p.sendAppend(records)
+	}
+}
+
+func (p *StreamProducer) sendAppend(records []*appendEntry) {
+	reqRecords := make([]*hstreampb.HStreamRecord, 0, len(records))
+	for _, record := range records {
+		reqRecords = append(reqRecords, record.value.ToHStreamRecord())
+	}
+	req := &hstreamrpc.Request{
+		Type: hstreamrpc.Append,
+		Req: &hstreampb.AppendRequest{
+			StreamName: p.streamName,
+			Records:    reqRecords,
+		},
+	}
+
+	// FIXME: add cache to avoid lookup zk every time
+	server, err := p.lookUp(context.Background(), p.streamName, p.key)
+	if err != nil {
+		handleBatchAppendError(err, records)
+		return
+	}
+
+	res, err := p.client.SendRequest(context.Background(), server, req)
+	if err != nil {
+		handleBatchAppendError(err, records)
+		return
+	}
+
+	size := len(records)
+	rids := res.Resp.(*hstreampb.AppendResponse).GetRecordIds()
+	for i := 0; i < size; i++ {
+		records[i].res.SetResponse(rids[i])
+	}
+}
+
+func (p *StreamProducer) lookUp(ctx context.Context, streamName string, key string) (string, error) {
+	address, err := randomServer(p.client)
 	if err != nil {
 		return "", err
 	}
@@ -136,23 +313,15 @@ func (s *Stream) LookUp(ctx context.Context, streamName string, key string) (str
 	}
 
 	var resp *hstreamrpc.Response
-	if resp, err = s.client.SendRequest(ctx, address, req); err != nil {
+	if resp, err = p.client.SendRequest(ctx, address, req); err != nil {
 		return "", err
 	}
 	node := resp.Resp.(*hstreampb.LookupStreamResponse).GetServerNode()
 	return fmt.Sprintf("%s:%d", node.GetHost(), node.GetPort()), nil
 }
 
-func (s *Stream) Append(ctx context.Context, streamName string, key string, tp client.RecordType, data []byte) {
-	//TODO implement me
-	panic("implement me")
-}
-
-func (s *Stream) randomServer() (string, error) {
-	infos, err := s.client.GetServerInfo()
-	idx := rand.Intn(len(infos))
-	if err != nil {
-		return "", err
+func handleBatchAppendError(err error, records []*appendEntry) {
+	for _, record := range records {
+		record.res.SetError(err)
 	}
-	return infos[idx], nil
 }
--- integraion_test/stream_test.go
+++ integraion_test/stream_test.go
@@ -5,10 +5,13 @@ import (
 	"client/hstream"
 	"client/util/test_util"
 	"context"
+	"fmt"
 	"github.com/stretchr/testify/suite"
 	"math/rand"
 	"strconv"
+	"sync"
 	"testing"
+	"time"
 )
 
 func TestStream(t *testing.T) {
@@ -36,6 +39,7 @@ func (s *testStreamSuite) TearDownTest() {
 
 func (s *testStreamSuite) TestCreateStream() {
 	ctx := context.Background()
+	rand.Seed(time.Now().UnixNano())
 	streamName := "test_stream_" + strconv.Itoa(rand.Int())
 	err := s.stream.Create(ctx, streamName, 1)
 	defer func() {
@@ -48,6 +52,7 @@ func (s *testStreamSuite) TestCreateStream() {
 
 func (s *testStreamSuite) TestDeleteStream() {
 	ctx := context.Background()
+	rand.Seed(time.Now().UnixNano())
 	streamName := "test_stream_" + strconv.Itoa(rand.Int())
 	err := s.stream.Create(ctx, streamName, 1)
 	s.NoError(err)
@@ -57,6 +62,7 @@ func (s *testStreamSuite) TestDeleteStream() {
 
 func (s *testStreamSuite) TestListStreams() {
 	ctx := context.Background()
+	rand.Seed(time.Now().UnixNano())
 	streams := make([]string, 0, 5)
 	for i := 0; i < 5; i++ {
 		streamName := "test_stream_" + strconv.Itoa(rand.Int())
@@ -76,4 +82,100 @@ func (s *testStreamSuite) TestListStreams() {
 		streamName := iter.Item().GetStreamName()
 		s.Contains(streams, streamName)
 	}
+}
+
+func (s *testStreamSuite) TestAppend() {
+	ctx := context.Background()
+	rand.Seed(time.Now().UnixNano())
+	streamName := "test_stream_" + strconv.Itoa(rand.Int())
+	err := s.stream.Create(ctx, streamName, 1)
+	defer func() {
+		_ = s.stream.Delete(ctx, streamName)
+	}()
+	s.NoError(err)
+
+	producer := s.stream.MakeProducer(streamName, "test-key")
+	res := make([]client.AppendResult, 0, 100)
+	for i := 0; i < 100; i++ {
+		r := producer.Append(client.RAWRECORD, []byte("test-value"))
+		res = append(res, r)
+	}
+
+	for _, r := range res {
+		resp, err := r.Ready()
+		s.NoError(err)
+		s.T().Log(resp.String())
+	}
+}
+
+func (s *testStreamSuite) TestBatchAppend() {
+	ctx := context.Background()
+	rand.Seed(time.Now().UnixNano())
+	streamName := "test_stream_" + strconv.Itoa(rand.Int())
+	err := s.stream.Create(ctx, streamName, 1)
+	defer func() {
+		_ = s.stream.Delete(ctx, streamName)
+	}()
+	s.NoError(err)
+
+	producer := s.stream.MakeProducer(streamName, "test-key", hstream.EnableBatch(10))
+	defer producer.Stop()
+
+	res := make([]client.AppendResult, 0, 100)
+	for i := 0; i < 100; i++ {
+		r := producer.Append(client.RAWRECORD, []byte("test-value"+strconv.Itoa(i)))
+		res = append(res, r)
+	}
+
+	for idx, r := range res {
+		resp, err := r.Ready()
+		s.NoError(err)
+		s.T().Log(fmt.Sprintf("record[%d]=%s", idx, resp.String()))
+	}
+}
+
+func (s *testStreamSuite) TestBatchAppendMultiKey() {
+	ctx := context.Background()
+	rand.Seed(time.Now().UnixNano())
+	streamName := "test_stream_" + strconv.Itoa(rand.Int())
+	err := s.stream.Create(ctx, streamName, 1)
+	defer func() {
+		_ = s.stream.Delete(ctx, streamName)
+	}()
+	s.NoError(err)
+
+	producer1 := s.stream.MakeProducer(streamName, "test-key1", hstream.EnableBatch(10))
+	defer producer1.Stop()
+	producer2 := s.stream.MakeProducer(streamName, "test-key2", hstream.EnableBatch(10))
+	defer producer2.Stop()
+	producer3 := s.stream.MakeProducer(streamName, "test-key3", hstream.EnableBatch(10))
+	defer producer3.Stop()
+
+	producers := []client.StreamProducer{producer1, producer2, producer3}
+	rids := sync.Map{}
+	wg := sync.WaitGroup{}
+	wg.Add(3)
+	for pid, p := range producers {
+		go func(pid int, p client.StreamProducer) {
+			result := make([]client.AppendResult, 0, 100)
+			for i := 0; i < 100; i++ {
+				r := p.Append(client.RAWRECORD, []byte(fmt.Sprintf("test-value-%d-%d", pid, i)))
+				result = append(result, r)
+			}
+			rids.Store(pid, result)
+			wg.Done()
+		}(pid, p)
+	}
+
+	wg.Wait()
+	rids.Range(func(key, value interface{}) bool {
+		pid := key.(int)
+		res := value.([]client.AppendResult)
+		for idx, r := range res {
+			resp, err := r.Ready()
+			s.NoError(err)
+			s.T().Log(fmt.Sprintf("[producer%d]: record[%d]=%s", pid, idx, resp.String()))
+		}
+		return true
+	})
 }
