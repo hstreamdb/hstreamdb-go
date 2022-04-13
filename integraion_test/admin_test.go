ea73e2ac15e94f53103c0e987a1c2c2366675174
YangKian <45479280+YangKian@users.noreply.github.com>
2022-04-13 10:25:02 +0800

    feat: add admin rpc (#16)

---
+++ integraion_test/admin_test.go
@@ -0,0 +1,43 @@
+package integraion
+
+import (
+	"github.com/hstreamdb/hstreamdb-go/hstream"
+	"github.com/hstreamdb/hstreamdb-go/util/test_util"
+	"github.com/stretchr/testify/suite"
+	"testing"
+)
+
+func TestAdmin(t *testing.T) {
+	suite.Run(t, new(testAdminSuite))
+}
+
+type testAdminSuite struct {
+	suite.Suite
+	serverUrl string
+	client    *hstream.HStreamClient
+}
+
+func (s *testAdminSuite) SetupTest() {
+	var err error
+	s.serverUrl = test_util.ServerUrl
+	s.client, err = hstream.NewHStreamClient(s.serverUrl)
+	s.NoError(err)
+}
+
+func (s *testAdminSuite) TearDownTest() {
+	s.client.Close()
+}
+
+func (s *testAdminSuite) TestGetStatus() {
+	cmd := "server status"
+	res, err := s.client.AdminRequest(cmd)
+	s.NoError(err)
+	s.T().Log(res)
+}
+
+func (s *testAdminSuite) TestStatsAppends() {
+	cmd := "server stats appends -i 1s -i 10s"
+	res, err := s.client.AdminRequest(cmd)
+	s.NoError(err)
+	s.T().Log(res)
+}
